6be56f930770b9246fc6340801fd79a0093d3413
Afiya Borkar <afiyaborkar@gmail.com>
2025-01-24 09:12:33 +0530

    remove port

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ nextApp.prepare().then(() => {
   });
 
   // Start server
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
